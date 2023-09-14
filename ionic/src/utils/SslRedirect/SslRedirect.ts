7fbaa9c9dc8d19ee97f9d160dd677dfb2eaf3574
Bruno Freire <brunoqm99@gmail.com>
2023-09-14 15:41:07 -0500

    Adjust deploy

--- ionic/src/utils/SslRedirect/SslRedirect.ts
+++ ionic/src/utils/SslRedirect/SslRedirect.ts
@@ -1,8 +1,6 @@
 class SslRedirect{
     public static listen(){
-        console.log("Listening");
         const location = new URL(window.location.href);
-        console.log(location);
         if (location.protocol != 'https:' && location.hostname != 'localhost') {
             //Redirect to same page but with https protocol, Change url and reload page:
             window.location.protocol = 'https';
