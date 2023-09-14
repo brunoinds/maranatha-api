fd3a59f3cc3261cdea2f57dc3a5a0f6b84f4e79d
Bruno Freire <brunoqm99@gmail.com>
2023-09-14 14:53:51 -0500

    Tests https online

--- ionic/src/utils/SslRedirect/SslRedirect.ts
+++ ionic/src/utils/SslRedirect/SslRedirect.ts
@@ -2,6 +2,7 @@ class SslRedirect{
     public static listen(){
         const location = new URL(window.location.href);
         if (location.protocol != 'https:' && location.hostname != 'localhost') {
+            console.log(location);
             //Redirect to same page but with https protocol, Change url and reload page:
             window.location.protocol = 'https';
         }
