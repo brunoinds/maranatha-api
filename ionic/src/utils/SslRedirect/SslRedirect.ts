1230b3cd7478d038a47e3ac526648149694a5d09
Bruno Freire <brunoqm99@gmail.com>
2023-09-14 14:52:01 -0500

    Corrects https redirection

--- ionic/src/utils/SslRedirect/SslRedirect.ts
+++ ionic/src/utils/SslRedirect/SslRedirect.ts
@@ -2,8 +2,8 @@ class SslRedirect{
     public static listen(){
         const location = new URL(window.location.href);
         if (location.protocol != 'https:' && location.hostname != 'localhost') {
-            //Redirect to same page but with https protocol:
-            location.protocol = 'https';
+            //Redirect to same page but with https protocol, Change url and reload page:
+            window.location.protocol = 'https';
         }
     }
 }
