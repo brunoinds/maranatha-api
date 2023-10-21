16c47d4a9df36dd4f2e1ad0966a496c52d17ef6c
Bruno Freire <brunoqm99@gmail.com>
2023-10-21 14:54:22 -0500

    R

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,8 +18,11 @@ class Cors
 
         return $next($request)
             ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            ->header('Access-Control-Allow-Methods','GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
         
+
+            
         $requestMethod = $request->server('REQUEST_METHOD');
         $requestOrigin = $request->server('HTTP_ORIGIN');
         $nextResponse = $next($request);
