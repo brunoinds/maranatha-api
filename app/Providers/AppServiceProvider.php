aa57b6e9409148d886a41fdc2a02f295bcdbd086
Bruno Freire <brunoqm99@gmail.com>
2024-03-26 17:02:00 -0400

    Update viewPulse gate definition

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Gate::define('viewPulse', function (User $user) {
+        Gate::define('viewPulse', function ($user = null) {
             return true;
         });
     }
