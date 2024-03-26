bdc7656530c45cfc21b594ca8e47d3493e98dd45
Bruno Freire <brunoqm99@gmail.com>
2024-03-26 08:02:39 -0400

    Add viewPulse gate to AppServiceProvider and uncomment Authorize middleware in pulse.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
+
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('viewPulse', function (User $user) {
+            return true;
+        });
     }
 }
