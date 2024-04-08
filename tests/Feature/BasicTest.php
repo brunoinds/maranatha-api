2918d083bd3574719fecdcec600b6f29ba64b852
Bruno Freire <brunoqm99@gmail.com>
2024-04-08 10:14:47 -0400

    Add Ionic project build steps to deploy workflow and fix storage link order

--- tests/Feature/BasicTest.php
+++ tests/Feature/BasicTest.php
@@ -12,8 +12,8 @@ class BasicTest extends TestCase
      */
     public function test_the_application_returns_a_successful_response(): void
     {
-        //$response = $this->get('/app');
-        //$response->assertStatus(200);
+        $response = $this->get('/app');
+        $response->assertStatus(200);
     }
 
     public function test_the_api_returns_a_successful_response(): void
