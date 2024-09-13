381b49e4d209a2f05d176a5e78ffd2b5924a7b80
Bruno Freire <brunoqm99@gmail.com>
2024-09-13 08:50:11 -0400

    Model ProjectJob, ProjectStructure

--- app/Models/ProjectJob.php
+++ app/Models/ProjectJob.php
@@ -4,8 +4,46 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\ProjectStructure;
+use App\Models\User;
+
+
 
 class ProjectJob extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'job_code',
+        'project_structure_id',
+        'width',
+        'height',
+        'area',
+        'admins_ids',
+        'supervisor_id',
+        'event_type',
+        'scheduled_start_date',
+        'scheduled_end_date',
+        'started_at',
+        'ended_at',
+        'status',
+        'final_report',
+        'messages',
+    ];
+
+    protected $casts = [
+        'admins_ids' => 'array',
+        'final_report' => 'array',
+        'messages' => 'array',
+    ];
+
+    public function projectStructure()
+    {
+        return $this->belongsTo(ProjectStructure::class);
+    }
+
+    public function admins()
+    {
+        return $this->hasMany(User::class, 'id', 'admins_ids');
+    }
 }
--- app/Models/ProjectStructure.php
+++ app/Models/ProjectStructure.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
 class ProjectStructure extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'structure_type',
+        'building_type',
+        'axes_count',
+        'beams_count',
+        'columns_count',
+        'stringers_count',
+        'facades_count',
+        'default_phases',
+    ];
+
+    protected $casts = [
+        'default_phases' => 'array',
+    ];
 }
