5c928457f6cdb6bba8bd3a091792456f4b5c1759
Bruno Freire <brunoqm99@gmail.com>
2024-12-02 17:01:37 -0400

    Add command to update inventory warehouse IDs for products

---
+++ app/Console/Commands/AddProductWarehouses.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\InventoryProduct;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Models\InventoryProductItem;
+
+class FixOrfanateProductItems extends Command
+{
+    protected $signature = 'inventory:add-product-warehouses';
+
+    protected $description = 'Description';
+
+    public function handle()
+    {
+        InventoryProduct::each(function ($product) {
+            $product->update([
+                'inventory_warehouses_ids' => [1, 2, 3, 5, 6, 7, 8, 9, 10]
+            ]);
+        });
+    }
+
+}
