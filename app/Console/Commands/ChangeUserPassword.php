5cd74ff78530996ce20f30f83f889aa1531200d0
Bruno Freire <brunoqm99@gmail.com>
2024-12-31 09:42:33 -0300

    Add command to change user password with interactive selection

---
+++ app/Console/Commands/ChangeUserPassword.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\InventoryProduct;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use App\Models\InventoryProductItem;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+
+class ChangeUserPassword extends Command
+{
+    protected $signature = 'users:change-password';
+
+    protected $description = 'Change user password';
+
+    public function handle()
+    {
+
+        $choice = $this->choice('📋 Choose an user to change the password:', array_map(function ($user) {
+            return $user['name'] . ' [' . $user['id'] . ']';
+        }, User::all()->toArray()));
+
+        $userId = str_replace(']', '', explode('[', $choice)[1]);
+
+        $user = User::where('id', $userId)->first();
+
+        // If user not found, display error message:
+        if (!$user) {
+            $this->error('❌ User not found');
+            return;
+        }
+
+        // Interrogate to enter new password:
+        $password = $this->secret('💬 Enter new password for ' . $user->name);
+
+
+        // Update user password:
+        User::where('id', $userId)->update([
+            'password' => Hash::make($password)
+        ]);
+
+        $this->info('✅ Password changed successfully');
+    }
+
+}
--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,7 @@ use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 
 class AuthController extends Controller{
